5ea0e567a9eca809f6a9f0cc2c70b481c5470ab1
beginor <beginor@qq.com>
2012-06-08 16:13:43 +0800

    edit main page gridpage default.html

---
+++ test/Modules/GridModule/Category.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace GridModule {
+
+	public class Category {
+
+		public string CategoryId {
+			get;
+			set;
+		}
+
+		public string CategoryName {
+			get;
+			set;
+		}
+
+		public string Description {
+			get;
+			set;
+		}
+
+	}
+
+	public class CategoryCollection : List<Category> {
+
+	}
+}
